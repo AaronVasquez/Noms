aa890914ba70271273e669648c926801610173c6
Aaron Vasquez <aaronvasquez1@gmail.com>
2012-04-08 18:25:52 -0400

    set up table view

---
+++ Noms/NOMRestaurantsViewController.h
@@ -0,0 +1,15 @@
+//
+//  NOMRestaurantsViewController.h
+//  Noms
+//
+//  Created by Aaron Vasquez on 4/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NOMRestaurantsViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
+@end
