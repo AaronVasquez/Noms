a877b5b7813d46cb3068ef252c9c4e4a91637193
Aaron Vasquez <aaronvasquez1@gmail.com>
2012-04-17 23:40:23 -0400

    prepared for adding menu

---
+++ Noms/NOMMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  NOMMenuViewController.h
+//  Noms
+//
+//  Created by Aaron Vasquez on 4/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NOMMenuViewController : UITableViewController
+
+@end
